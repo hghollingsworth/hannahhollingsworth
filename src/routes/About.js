f0122027ae95472ae9b0469b60b0b5fd68fe1f8b
hghollingsworth <h.g.hollingsworth@wustl.edu>
2024-11-21 21:55:09 -0600

    components

---
+++ src/routes/About.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import "./About.css"
+
+
+const About = () => {
+  return (
+    <div>
+      <h1>Hi, I'm Hannah Hollingsworth (she/her)</h1>
+      <img src="https://media.licdn.com/dms/image/C4E03AQHps-X8VsP_ig/profile-displayphoto-shrink_400_400/0/1607361173494?e=1698278400&v=beta&t=HDyyEghzm_QfjiRxR8bk0qjCIOtX8ZzV06hqn-QdVmU" alt="Self" class="right" align="right"/>
+ 
+<p>Passionate engineer interested in a wide range of applications</p>
+<body>
+  
+  Currently, I am a junior at WashU in St. Louis studying computer science and physics. I am interested in front-end dev, software engineering, and love learning about space and the bounds of our universe. I am experienced with both front-end and back-end devlopment, and love applying my skills to a multitude of different problems.
+    I am experienced with Java, Matlab, HTML/CSS, SQL, Tableau, C++, and more.
+
+    So far in my academic journey, I have really enjoyed exploring all aspects of design and development, but I also am a creative human outside of that sphere. My involvement with WashU's Strike fashion magazine as a photoshoot director has been such a fun creative outlet, and you can always find me testing and developing new recipes as well!
+    <img src="https://drive.google.com/file/d/1fz0AFPSCd7tnNe7OSMEbU7mbmjsJ6NMP/view?usp=drive_link" alt="Hannah pic"></img>
+
+    
+
+ </body>
+ <ul className="contactme">
+        <li><u>
+          <a href="https://www.linkedin.com/in/hannah-hollingsworth-a9703b201">LinkedIn</a>
+        </u>
+        </li>
+        
+        <li>
+            email: hghollings@gmail.com
+        </li>   
+   
+        <li>
+            phone: (980)-215-4701
+        </li>
+
+        
+   </ul>
+   
+    </div>
+  )
+}
+
+export default About
